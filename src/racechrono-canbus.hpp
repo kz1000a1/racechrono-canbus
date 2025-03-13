6c7bed1146913684c5118d285d2d05bbe8d4e0df
kz1000a1 <146339266+kz1000a1@users.noreply.github.com>
2025-03-13 19:57:10 +0900

    Update racechrono-canbus.hpp

--- src/racechrono-canbus.hpp
+++ src/racechrono-canbus.hpp
@@ -77,6 +77,14 @@
 #define BLE_PWR_LVL ESP_PWR_LVL_P9
 #endif
 
+// M5Stack Atom CAN
+#if defined(ARDUINO_M5STACK_ATOM)
+#define LED_BUILTIN 13
+#define CAN_RX_PIN GPIO_NUM_19
+#define CAN_TX_PIN GPIO_NUM_22
+#define BLE_PWR_LVL ESP_PWR_LVL_P9
+#endif
+
 /// disable copy
 #define CPP_NOCOPY(_name)                     \
     _name(_name const&) = delete;             \
